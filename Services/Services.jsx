71ff17cd0be745807672107479704b38d137bcf4
AdarshFez <adarshfez@gmail.com>
2023-03-08 16:22:28 -0500

    Create Services.jsx
    making services

---
+++ Services/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const Services = {
+    
+}
+
