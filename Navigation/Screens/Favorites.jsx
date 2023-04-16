1da6e755e52131094de9c28cc17a13ddfdecacb7
AdarshFez <adarshfez@gmail.com>
2023-04-16 02:41:10 -0400

    adding other screens
    adding other screens

---
+++ Navigation/Screens/Favorites.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Button,
+  useColorScheme,
+  View,
+} from 'react-native';
+// Import Map and Marker
+import MapView, {Marker, enableLatestRenderer, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import MapScreen from './Navigation/Screens/MapScreen';
+
+const App = (props) =>{
+
+  
+
+  return(
+  <View style={styles.container}>
+      
+      <h1>"Hello, I'm Favoites"</h1>
+     
+   </View>)
+}
+
+
+ export default App;
+
+ const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent:'center',
+    flex: 1,
+  },
+  map: {
+    width:'100%',
+    height:'100%'
+  },
+ });
